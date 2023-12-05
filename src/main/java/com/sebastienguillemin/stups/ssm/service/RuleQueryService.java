d67dc2f06cc141bc48112ea86723547e07ab30b4
Sébastien GUILLEMIN <sebastien.guillemin11@gmail.com>
2023-12-05 14:34:39 +0100

    Vérification dosage produits de coupage

--- src/main/java/com/sebastienguillemin/stups/ssm/service/RuleQueryService.java
+++ src/main/java/com/sebastienguillemin/stups/ssm/service/RuleQueryService.java
@@ -62,6 +62,30 @@ public class RuleQueryService {
         + "    }\n"
         + "    BIND ((?countPC1 = ?countPC2 && ?countSB1 = ?countSB2 && ?countSB1 = ?commonSB) as ?match)\n"
         + "}";
+    
+    public final static String DOSAGE_CUTTING_PRODUCTS_RULE =
+          "PREFIX : <http://www.stups.fr/ontologies/2023/stups/>\n"
+        + "SELECT ?match\n"
+        + "WHERE {\n"
+        + "    {\n"
+        + "        SELECT ?dosageMatch\n"
+        + "        WHERE {\n"
+        + "            ?s1 a :Echantillon .\n"
+        + "            ?s2 a :Echantillon .\n"
+        + "            ?s1 :id \"%s\" .\n"
+        + "            ?s2 :id \"%s\" .\n"
+        + "            ?s1 :aProduitCoupage ?pc1 .\n"
+        + "            ?pc1 :aSubstance ?sb .\n"
+        + "            ?pc1 :dosage ?d1 .\n"
+        + "            ?s2 :aProduitCoupage ?pc2 .\n"
+        + "            ?pc2 :aSubstance ?sb .\n"
+        + "            ?pc2 :dosage ?d2\n"
+        + "            BIND ((ABS(?d1 - ?d2) < 5) as ?dosageMatch)\n"
+        + "        }\n"
+        + "        GROUP BY ?dosageMatch\n"
+        + "    }\n"
+        + "    BIND ((?dosageMatch != false) as ?match)\n"
+        + "}";
 
     public final static String LOGO_RULE = 
             "PREFIX : <http://www.stups.fr/ontologies/2023/stups/>"
@@ -77,7 +101,7 @@ public class RuleQueryService {
         + "    ?s1 :nomLogo ?logoName1 .\n"
         + "    ?s2 :nomLogo ?logoName2 .\n"
         + "    BIND((?logo1 = ?logo2 && ?logoName1 = ?logoName2) as ?match)\n"
-        + "}\n";
+        + "}";
     
     public final static String CHIMICAL_FORM_RULE =
           "PREFIX : <http://www.stups.fr/ontologies/2023/stups/>"
@@ -93,7 +117,7 @@ public class RuleQueryService {
         + "    ?ap1 :aFormeChimique ?f1 .\n"
         + "    ?ap2 :aFormeChimique ?f2 .\n"
         + "    BIND ((?f1 = ?f2) as ?match)\n"
-        + "}\n";
+        + "}";
 
     public final static String DATE_RULE = 
           "PREFIX : <http://www.stups.fr/ontologies/2023/stups/>"
@@ -114,19 +138,23 @@ public class RuleQueryService {
         + "    bind ((?d1 - ?d2) as ?diff) .\n"
         + "    bind (if(?diff < \"P0Y0M0DT0H0M20.000S\"^^xsd:duration, (-1) * ?diff, ?diff) as ?absDiff) .\n"
         + "    bind ((?absDiff < \"P0Y6M0DT0H0M20.000S\"^^xsd:duration) as ?match) .\n"
-        + "}\n";
+        + "}";
 
     public List<RuleQuery> exectuteQueries(String sample1Id, String sample2Id) throws Exception {
         List<RuleQuery> queries = new ArrayList<>();
         RuleQuery typeQuery = new RuleQuery("Même types de drogue", String.format(DRUG_TYPE_RULE, sample1Id, sample2Id));
         RuleQuery cuttingProductsQuery = new RuleQuery("Même types de produits de coupage", String.format(CUTTING_PRODUCTS_RULE, sample1Id, sample2Id, sample1Id, sample2Id));
-        RuleQuery logoQuery = new RuleQuery("Même logos", String.format(LOGO_RULE, sample1Id, sample2Id));
+        RuleQuery dosageCuttingProductsQuery = new RuleQuery("Ecart de moins de 5% pour les dosages des produits de coupage", String.format(DOSAGE_CUTTING_PRODUCTS_RULE, sample1Id, sample2Id));
+        // TODO : ne tester le dosage des produits de coupage que s'il y ales mêmes produits de coupage.
+
+        // RuleQuery logoQuery = new RuleQuery("Même logos", String.format(LOGO_RULE, sample1Id, sample2Id));
         RuleQuery chilmicalFormQuery = new RuleQuery("Même formes chimiques", String.format(CHIMICAL_FORM_RULE, sample1Id, sample2Id));
         RuleQuery dateQuery = new RuleQuery("Dates de saisie éloignés de moins de 6 mois", String.format(DATE_RULE, sample1Id, sample2Id));
 
         queries.add(typeQuery);
         queries.add(cuttingProductsQuery);
-        queries.add(logoQuery);
+        queries.add(dosageCuttingProductsQuery);
+        // queries.add(logoQuery);
         queries.add(chilmicalFormQuery);
         queries.add(dateQuery);
         
