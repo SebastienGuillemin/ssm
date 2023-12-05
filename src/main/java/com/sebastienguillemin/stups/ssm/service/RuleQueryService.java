41e1642ea6fb1bef87816f5aea655a13505ce398
SebastienGuillemin <sebastien.guillemin11@gmail.com>
2023-12-05 08:51:19 +0100

    MAJ

--- src/main/java/com/sebastienguillemin/stups/ssm/service/RuleQueryService.java
+++ src/main/java/com/sebastienguillemin/stups/ssm/service/RuleQueryService.java
@@ -63,13 +63,72 @@ public class RuleQueryService {
         + "    BIND ((?countPC1 = ?countPC2 && ?countSB1 = ?countSB2 && ?countSB1 = ?commonSB) as ?match)\n"
         + "}";
 
+    public final static String LOGO_RULE = 
+            "PREFIX : <http://www.stups.fr/ontologies/2023/stups/>"
+        + "SELECT ?match\n"
+        + "WHERE\n"
+        + "{\n"
+        + "    ?s1 a :Echantillon .\n"
+        + "    ?s2 a :Echantillon .\n"
+        + "    ?s1 :id \"%s\" .\n"
+        + "    ?s2 :id \"%s\" .\n"
+        + "    ?s1 :logo ?logo1 .\n"
+        + "    ?s2 :logo ?logo2 .\n"
+        + "    ?s1 :nomLogo ?logoName1 .\n"
+        + "    ?s2 :nomLogo ?logoName2 .\n"
+        + "    BIND((?logo1 = ?logo2 && ?logoName1 = ?logoName2) as ?match)\n"
+        + "}\n";
+    
+    public final static String CHIMICAL_FORM_RULE =
+          "PREFIX : <http://www.stups.fr/ontologies/2023/stups/>"
+        + "SELECT ?match\n"
+        + "WHERE\n"
+        + "{\n"
+        + "    ?s1 a :Echantillon .\n"
+        + "    ?s2 a :Echantillon .\n"
+        + "    ?s1 :id \"%s\" .\n"
+        + "    ?s2 :id \"%s\" .\n"
+        + "    ?s1 :aPrincipeActif ?ap1 .\n"
+        + "    ?s2 :aPrincipeActif ?ap2 .\n"
+        + "    ?ap1 :aFormeChimique ?f1 .\n"
+        + "    ?ap2 :aFormeChimique ?f2 .\n"
+        + "    BIND ((?f1 = ?f2) as ?match)\n"
+        + "}\n";
+
+    public final static String DATE_RULE = 
+          "PREFIX : <http://www.stups.fr/ontologies/2023/stups/>"
+        + "SELECT ?match\n"
+        + "WHERE\n"
+        + "{\n"
+        + "    ?s1 a :Echantillon .\n"
+        + "    ?s2 a :Echantillon .\n"
+        + "    ?s1 :id \"%s\" .\n"
+        + "    ?s2 :id \"%s\" .\n"
+        + "    ?s1 :provientDe ?se1 .\n"
+        + "    ?se1 :estDansSaisine ?sa1 .\n"
+        + "    ?sa1 :date ?d1 .\n"
+        + "    \n"
+        + "    ?s2 :provientDe ?se2 .\n"
+        + "    ?se2 :estDansSaisine ?sa2 .\n"
+        + "    ?sa2 :date ?d2 .\n"
+        + "    bind ((?d1 - ?d2) as ?diff) .\n"
+        + "    bind (if(?diff < \"P0Y0M0DT0H0M20.000S\"^^xsd:duration, (-1) * ?diff, ?diff) as ?absDiff) .\n"
+        + "    bind ((?absDiff < \"P0Y6M0DT0H0M20.000S\"^^xsd:duration) as ?match) .\n"
+        + "}\n";
+
     public List<RuleQuery> exectuteQueries(String sample1Id, String sample2Id) throws Exception {
         List<RuleQuery> queries = new ArrayList<>();
-        RuleQuery typeQuery = new RuleQuery("Type de drogue", String.format(DRUG_TYPE_RULE, sample1Id, sample2Id));
-        RuleQuery cuttingProductsQuery = new RuleQuery("Type de produits de coupage", String.format(CUTTING_PRODUCTS_RULE, sample1Id, sample2Id, sample1Id, sample2Id));
+        RuleQuery typeQuery = new RuleQuery("Même types de drogue", String.format(DRUG_TYPE_RULE, sample1Id, sample2Id));
+        RuleQuery cuttingProductsQuery = new RuleQuery("Même types de produits de coupage", String.format(CUTTING_PRODUCTS_RULE, sample1Id, sample2Id, sample1Id, sample2Id));
+        RuleQuery logoQuery = new RuleQuery("Même logos", String.format(LOGO_RULE, sample1Id, sample2Id));
+        RuleQuery chilmicalFormQuery = new RuleQuery("Même formes chimiques", String.format(CHIMICAL_FORM_RULE, sample1Id, sample2Id));
+        RuleQuery dateQuery = new RuleQuery("Dates de saisie éloignés de moins de 6 mois", String.format(DATE_RULE, sample1Id, sample2Id));
 
         queries.add(typeQuery);
         queries.add(cuttingProductsQuery);
+        queries.add(logoQuery);
+        queries.add(chilmicalFormQuery);
+        queries.add(dateQuery);
         
         for (RuleQuery ruleQuery : queries) {
             boolean executionComplete = this.repository.execute(ruleQuery);
